6b05d2b613c6fc10864da111fd323507aa19c1a9
Tejan <tej780@hotmail.co.uk>
2019-05-18 14:02:53 +0100

    Fix api endpoint /data.json

--- backend/src/app.py
+++ backend/src/app.py
@@ -3,11 +3,17 @@
 app.py - Create web server
 """
 
-from flask import Flask
+from flask import Flask, jsonify
+import json
 
+app = Flask(__name__, static_folder="./models", static_url_path="")
 
-def create_app():
-    """Initialise flask web server
-    """
-    app = Flask(__name__, static_folder="./models", static_url_path="")
-    return app
+@app.route('/data.json')
+def api():
+    
+    with open('./src/data/data.json') as file:
+        data = json.load(file)
+    data = str(data)
+    return jsonify(dict(tiles=data))
+
+app.run(host='0.0.0.0', port='5000')
